4988fe4e165d3374dbc93eb1c784bd356364372c
Alexandre Angelim <angelim@angelim.com.br>
2012-04-19 07:01:32 -0300

    moving code from target application to gem

--- config/initializers/brcobranca_encoding.rb
+++ config/initializers/brcobranca_encoding.rb
@@ -1,3 +1,6 @@
-# Brcobranca.setup do |config|
-#   config.external_encoding = 'ascii-8bit'
-# end
+Brcobranca.setup do |config|
+  config.external_encoding = 'ascii-8bit'
+  config.resolucao = 300
+end
+RGhost::Config::GS[:external_encoding] = 'ascii-8bit'
+RGhost::Config::GS[:path] = `which gs`.strip
